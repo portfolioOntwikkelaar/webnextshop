210373331f2c2a82a63703cc8238ec236e4a89d5
noordseoostende <voropayev1@mail.ru>
2021-09-07 21:53:28 +0200

    layout

---
+++ components/ui/Marquee/Marquee.tsx
@@ -0,0 +1,21 @@
+import { ReactNode, FC } from "react"
+import style from './Marquee.module.css'
+
+interface Props {
+  children: ReactNode[]
+}
+
+const Marquee: FC<Props> = ({children}) => {
+
+  return (
+
+    <div className={style.root}>
+      <div className={style.container}>
+
+      {children}
+      </div>
+    </div>
+  )
+}
+
+export default Marquee
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import getAllProducts from '@framework/product/get-all-products'
 import { getConfig } from "@framework/api/config"
 import { Layout } from "@components/common"
 import { ProductCard } from "@components/product"
-import { Grid, Hero } from "@components/ui"
+import { Grid, Hero, Marquee } from "@components/ui"
 
 export async function getStaticProps() {
   const config = getConfig()
@@ -39,6 +39,16 @@ export default function Home({
         headline="Lorem ipsum, ice cream"
         description="Dragée cake gummi bears tiramisu toffee oat cake bear claw caramels. Tart halvah sesame snaps tootsie roll caramels ice cream donut danish. Sesame snaps chocolate jujubes jelly gummies oat cake brownie bear claw fruitcake. Tiramisu macaroon pie cotton candy bear claw. Topping chocolate chocolate bar dessert pastry cotton candy apple pie apple pie bonbon. Toffee candy canes pudding chocolate bar apple pie. Brownie croissant oat cake croissant halvah pastry. Sweet roll jelly-o danish bear claw cheesecake pastry chocolate cake jujubes biscuit."   
       />
+      <Marquee>
+      { products.slice(0, 3).map(product => 
+        <ProductCard 
+          key={product.id}
+          variant="slim"
+          product={product}  
+          />
+
+        )}
+      </Marquee>
     </>
   )
 }
