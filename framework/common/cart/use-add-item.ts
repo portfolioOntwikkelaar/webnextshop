bb5f3e1a01d5009517f5dabf2df2282663f17615
noordseoostende <voropayev1@mail.ru>
2021-10-23 21:06:52 +0200

    24

--- framework/common/cart/use-add-item.ts
+++ framework/common/cart/use-add-item.ts
@@ -1,13 +1,18 @@
 
-
 import { useHook } from "@common/utils/use-hook"
+import { handler } from "@framework/cart/use-add-item"
+// import { useHook } from "@common/utils/use-hook"
 
   const useAddItem = () => {
 
     const hook = useHook((hooks) => {
       return hooks.cart.useAddItem})
 
-  return hook.useHook()
+  return hook.useHook({
+    fetch: hook.fetcher
+  })
 } 
+
+
 
 export default useAddItem
